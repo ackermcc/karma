38363b59774047409495068d11f9cd6794b702bf
Chad Ackerman <chad.ackerman@nitelifecam.comgit config --global user.email ackermcc@gmail.com>
2015-05-06 21:55:40 -0700

    init

---
+++ Karma/Karma/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Karma
+//
+//  Created by Chad Ackerman on 5/6/15.
+//  Copyright (c) 2015 Chad Ackerman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
